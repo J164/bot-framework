005cb5c69948e05936c6dce07f9d9ad1aff99beb
Jacob Edley <jac.dan@icloud.com>
2023-08-13 14:26:49 -0500

    set up entry point

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { startBot } from './bot-client.js';
+export { deployApplicationCommands } from './deploy.js';
+export { fetchCollection } from './database.js';
+export { EmbedType, Emojis, BotColors, responseEmbed, responseOptions } from './util/response-helpers.js';
+export { DestinationType, sendPaginatedMessage } from './util/message-component-helpers.js';
